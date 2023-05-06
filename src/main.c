2edec02691bce72674a7efce31dd070e91c3beb6
HalfSweet <HalfSweet@HalfSweet.cn>
2023-05-06 21:27:24 +0800

    fix: 修改RTT的缓冲区大小，并使用MtFmT进行格式化字符串

--- src/main.c
+++ src/main.c
@@ -23,8 +23,8 @@
 /* USER CODE BEGIN Includes */
 #include "stdint.h"
 #include "string.h"
-//#include "SEGGER_RTT.h"
-//#include "mm_fmt.h"
+#include "SEGGER_RTT.h"
+#include "mm_fmt.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -90,16 +90,16 @@ int main(void)
   HS_GPIO_Init();
   HS_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-  // SEGGER_RTT_Init();
-  // SEGGER_RTT_printf(0, "Hello world ! \n");
+  SEGGER_RTT_Init();
+  SEGGER_RTT_printf(0, "Hello world ! \n");
 
-  // MString str;
-  // mstr_create_empty(&str);
-  // mstr_format("Num:{0:u32} \n", 
-  // &str, 
-  // 1, 
-  // 114514u);
-  // SEGGER_RTT_printf(0, "%s", mstr_as_cstr(&str));
+  MString str;
+  mstr_create_empty(&str);
+  mstr_format("Num:{0:u32} \n", 
+  &str, 
+  1, 
+  114514u);
+  SEGGER_RTT_WriteString(0, mstr_as_cstr(&str));
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -111,6 +111,9 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
     // SEGGER_RTT_printf(0, "The NO.%d print. \n", count);
+    mstr_clear(&str);
+    mstr_format("The NO.{0:u32} print. \n", &str, 1, count);
+    SEGGER_RTT_WriteString(0, mstr_as_cstr(&str));
     count++;
     HAL_Delay(1000);
   }
